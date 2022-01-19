ba6c4965292b40928af51ba14b20eb44d40d1538
snowroll <chaihj15@163.com>
2022-01-19 21:07:46 +0800

    badpkg: v1.0.0

--- badpkg.go
+++ badpkg.go
@@ -1,6 +1,5 @@
 package badpkg
 import "fmt"
-import "rsc.io/quote"
 
 func Hello(name string) string {
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
@@ -8,6 +7,6 @@ func Hello(name string) string {
 }
 
 func Test() string {
-    
-    return quote.Hello()
+    msg := 'hhh'
+    return msg
 }

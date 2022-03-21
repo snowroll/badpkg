e6c0d4de6f562fb6021f6442c59896cd2912b0df
snowroll <chaihj15@163.com>
2022-03-21 10:20:20 +0800

    badpkg:v1.0.3

--- badpkg.go
+++ badpkg.go
@@ -2,7 +2,7 @@ package badpkg
 import "fmt"
 
 func Hello(name string) string {
-    message := fmt.Sprintf("Hi, %v. Welcome! the version of badpkg is v1.0.2", name)
+    message := fmt.Sprintf("Hi, %v. Welcome! the version of badpkg is v1.0.3", name)
     return message
 }
 
